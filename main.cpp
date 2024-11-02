3b00f79cc0687b32a775339de36bba30b71ffee4
lhj67611 <1556626415@qq.com>
2024-11-02 12:03:53 +0800

    Accept:对main文件中代码进行反向缩进

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 	vector<int> vec_underlying_all = {1,2,3,4,5};
-	for(int i = 0;i < vec_underlying_all.size();i++) {
+   for(int i = 0;i < vec_underlying_all.size();i++) {
 		if(i != vec_underlying_all.size-1) {
 			cout << vec_underlying_all[i] << ",";
 		} else {
